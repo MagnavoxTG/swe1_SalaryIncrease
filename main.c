8b2a59481b493ac826f9658158744985e1b8e04f
MagnavoxTG <dominik.lohner92@gmx.at>
2019-10-25 10:39:05 +0200

    Version 1.1
    Changelog:
    Changed some comments in the code to complete the documentation.

--- main.c
+++ main.c
@@ -8,24 +8,29 @@ void main() {
     float number = 0;
 
     while (x != 1) {
-        // printf shows a message asking you to enter a positive number
+        // printf shows a message asking you to enter an employees salary
         printf("\nPlease enter the current salary of an employee (f.e. 1650.80): ");
         //scanf writes the input into a string (char array)
         scanf("%s", floatreadout);
-        //gets(intreadout);
         // strspan checks how many of the specified character are inside the string and gives an integer
         // strlen checks how long the string (char array) is and gives an integer
         // if they are the same then there are only allowed characters in the string
         if (strspn(floatreadout, "0123456789.") == strlen(floatreadout)) {
+            // the string gets cast into a float
             number = (float)atof(floatreadout);
+            // if the number is bigger than 1000 it will increase the salary by 0.85%
             if (number > 1000) {
                 number = number * 1.0085;
+            // if the number is less than 1000 it will increase the salary by 2.09%
             } else {
                 number = number * 1.0209;
             }
+            // showing the new salary
             printf("The new salary is: %.2f", number);
+            //breaking the while
             x = 1;
         } else {
+            // displaying error message for using invalid characters in the input
             printf("invalid input");
         }
     }
